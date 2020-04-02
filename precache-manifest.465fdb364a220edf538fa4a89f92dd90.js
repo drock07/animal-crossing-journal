381cbe978d9825b5b4e0cd3930533f7791554a86
David Rockwood <david@edrawapp.com>
2020-04-02 17:39:16 -0600

    Updates

---
+++ precache-manifest.465fdb364a220edf538fa4a89f92dd90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be22d84534e922ff981d8882ab49eb5b",
+    "url": "/animal-crossing-journal/index.html"
+  },
+  {
+    "revision": "c0e2c13849e7879ba5a1",
+    "url": "/animal-crossing-journal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1503a779f3056e8b707",
+    "url": "/animal-crossing-journal/static/js/2.e5555304.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animal-crossing-journal/static/js/2.e5555304.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e2c13849e7879ba5a1",
+    "url": "/animal-crossing-journal/static/js/main.d88f3038.chunk.js"
+  },
+  {
+    "revision": "704d686f2d805df8a841",
+    "url": "/animal-crossing-journal/static/js/runtime-main.cf4f27b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/animal-crossing-journal/static/media/logo.5d5d9eef.svg"
+  }
+]);
